2ee483a6c11aa348df9411d59e10cbcce91e6842
Miguel Vivan <miguel.farinav@gmail.com>
2026-02-09 22:53:02 -0300

    Adição das novas tarefas das aulas

---
+++ 5_bind_mount_compose/projeto/flask/app.py
@@ -0,0 +1,38 @@
+import flask
+from flask import jsonify
+import flask_mysqldb
+from flask_mysqldb import MySQL
+import requests
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+app.config['MYSQL_HOST'] = 'db'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flaskdocker'
+
+mysql = MySQL(app)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    data = requests.get("https://randomuser.me/api")
+    return data.json()
+
+
+@app.route("/inserthost", methods=["POST"])
+def inserthost():
+    data = requests.get("https://randomuser.me/api").json()
+    username = data['results'][0]['name']['first']
+
+    curl = mysql.connection.cursor()
+    curl.execute(""" INSERT INTO users(name) VALUES(%s)""", (username,))
+    mysql.connection.commit()
+    curl.close()
+
+    return "Boa tarde, " + username
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True, port="5000")
+    
